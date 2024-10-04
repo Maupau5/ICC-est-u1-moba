e82af864b87f71f0100508a2d8a702531f42b786
Maupau5 <mauroipaul@gmail.com>
2024-10-04 13:04:48 -0500

    BurbujaAjuste

--- src/App.java
+++ src/App.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         MetodoOrdenamiento mo = new MetodoOrdenamiento();
-        Scanner scanner = new Scanner(System.in);
-        int tamanio = 0;
+        Scanner sc = new Scanner(System.in);
+        int tamanio = leerEnteroValido(sc, "Ingrese el tamanio del arreglo", false);
 
-        do { 
+        do {
             System.out.println("Ingrese el tamaño del arreglo:");
-            while (!scanner.hasNextInt()) {
+            while (!sc.hasNextInt()) {
                 System.out.println("Ingrese correctamente un número entero:");
-                scanner.next();  
+                sc.next();
             }
-            tamanio = scanner.nextInt();
+            tamanio = sc.nextInt();
             if (tamanio <= 0) {
                 System.out.println("El tamaño debe ser un entero positivo.");
             }
@@ -23,11 +23,11 @@ public class App {
         System.out.println("Ingrese los elementos del arreglo:");
         for (int i = 0; i < tamanio; i++) {
             System.out.print("Elemento " + (i + 1) + ": ");
-            while (!scanner.hasNextInt()) {
+            while (!sc.hasNextInt()) {
                 System.out.println("Ingrese correctamente un número entero:");
-                scanner.next();  
+                sc.next();
             }
-            arreglo[i] = scanner.nextInt();
+            arreglo[i] = sc.nextInt();
         }
 
         arreglo = mo.BurbujaAjuste(arreglo);
@@ -35,6 +35,26 @@ public class App {
         System.out.println("Arreglo ordenado:");
         mo.printArreglo(arreglo);
 
-        scanner.close();
+        sc.close();
+    }
+    
+
+    public static int leerEnteroValido(Scanner sc, String mensaje, boolean permitirNegativos) {
+        int numero;
+        do {
+            System.out.println(mensaje);
+            while (!sc.hasNextInt()) {
+                System.out.println("Ingrese correctamente un número entero:");
+                sc.next();
+            }
+            numero = sc.nextInt();
+
+            if (!permitirNegativos && numero < 0) {
+                System.out.println("No se permiten numeros negativos. Ingrese un numero entero positivo");
+            }
+        } while (permitirNegativos && numero < 0);
+
+        return numero;
+
     }
 }
